a6088ec467ef7bb7d0c0df3f039a5af23a9a0629
akkqp <greenpurslane+gthb@gmail.com>
2017-04-27 13:52:12 +0900

    勝どき行き
    アイコン差し替え(28→25)

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,7 @@ var main = new UI.Card({
   style: 'large'
 });
 
-var url='http://www.jorudan.co.jp/norikae/cgi/nori.cgi?eki1=%E5%A4%A7%E5%B4%8E&eki2=%E4%BA%80%E6%88%B8&eki3=%E7%A7%8B%E8%91%89%E5%8E%9F&via_on=1&Cway=0&Czu=2&C7=1&C2=0&C3=0&C1=0&C4=0&C6=2&S.x=78&S.y=14&S=%E6%A4%9C%E7%B4%A2&Cmap1=&rf=nr&pg=0&eok1=R-&eok2=R-&eok3=R-&Csg=1&type=t&Cid=0&Cfp=1';
+var url='http://www.jorudan.co.jp/norikae/cgi/nori.cgi?eki1=%E5%8B%9D%E3%81%A9%E3%81%8D&eki2=%E6%9D%B1%E5%A4%A7%E5%B3%B6&Cway=0&Czu=2&C7=1&C2=0&C3=0&C1=0&C4=0&C6=2&S.x=78&S.y=14&S=%E6%A4%9C%E7%B4%A2&Cmap1=&rf=nr&pg=0&eok1=R-&eok2=R-&eok3=R-&Csg=1&type=t&Cid=0&Cfp=1';
 
 var parseResult = function(url){
   var tmpResult = 'updating...';
